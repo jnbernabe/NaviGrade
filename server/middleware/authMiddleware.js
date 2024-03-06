b3397cb4e5fc6b3e4d43f01f29f266298ae92355
Jamaal Bernabe <85321486+jnbernabe@users.noreply.github.com>
2024-03-06 18:20:05 -0500

    Changes to NavBar, Work on refreshToken, new packages installed on server

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,33 +1,39 @@
 // authMiddleware.js
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+const client = process.env.CLIENT_URL;
 
 const authenticateToken = (req, res, next) => {
-  const token = req.header('Authorization');
+  const token = req.header("Authorization");
   //console.log('Received token:', token);
 
   if (!token) {
-    console.error('Access denied: No token provided');
-    return res.status(401).json({ message: 'Access denied' });
+    console.error("Access denied: No token provided");
+    return res.status(401).json({ message: "Access denied" });
   }
 
-  const secretKey = process.env.JWT_SECRET || 'Zht6Nvy7Yr9Qp7KAlAIU8GzPqLUrzTSK';
+  const secretKey =
+    process.env.JWT_SECRET || "Zht6Nvy7Yr9Qp7KAlAIU8GzPqLUrzTSK";
 
   // Remove the 'Bearer ' prefix and trim whitespaces
-  const cleanedToken = token.replace('Bearer ', '').trim();
+  const cleanedToken = token.replace("Bearer ", "").trim();
 
   jwt.verify(cleanedToken, secretKey, (err, decoded) => {
     if (err) {
       // Log the decoded token for further inspection
-      console.error('Decoded token:', decoded);
-      console.error('Error during verification:', err);
+      console.error("Decoded token:", decoded);
+      console.error("Error during verification:", err);
 
-      if (err.name === 'TokenExpiredError') {
-        console.error('Token has expired');
-        return res.status(403).json({ message: 'Token has expired' });
+      if (err.name === "TokenExpiredError") {
+        console.error("Token has expired. Redirecting to login page.");
+        return res.redirect(client + "/logout");
       }
 
-      console.error('Invalid token:', err.message);
-      return res.status(403).json({ message: 'Invalid token', error: err.message });
+      console.error("Invalid token:", err.message);
+      return res
+        .status(403)
+        .json({ message: "Invalid token", error: err.message });
     }
 
     req.user = decoded;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,42 +1,44 @@
 const express = require("express");
 const ObjectId = require("mongodb").ObjectId;
 const Student = require("../models/Student.js");
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 
 const tokenBlacklist = [];
 
 // Middleware to check if the token is blacklisted
 const checkTokenBlacklist = (req, res, next) => {
-  const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
-  if (token  && tokenBlacklist.includes(token)) {
-    return res.status(401).json({ message: 'Token revoked' });
+  const token =
+    req.headers.authorization && req.headers.authorization.split(" ")[1];
+  if (token && tokenBlacklist.includes(token)) {
+    return res.status(401).json({ message: "Token revoked" });
   }
   // Token is not blacklisted, continue processing the request
   next();
 };
 
 // Logout route
-router.post('/logout', checkTokenBlacklist, (req, res) => {
-   const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
+router.post("/logout", checkTokenBlacklist, (req, res) => {
+  const token =
+    req.headers.authorization && req.headers.authorization.split(" ")[1];
   if (!token) {
-    return res.status(401).json({ message: 'Invalid token' });
+    return res.status(401).json({ message: "Invalid token" });
   }
   // Add the token to the blacklist
   tokenBlacklist.push(token);
-  res.json({ message: 'Logout successful' });
+  res.json({ message: "Logout successful" });
 });
 
 // Signup route for students
-router.post('/signup', async (req, res) => {
+router.post("/signup", async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
 
   try {
     // Check if the email is already registered
     const existingStudent = await Student.findOne({ email });
     if (existingStudent) {
-      return res.status(400).json({ message: 'Email is already registered' });
+      return res.status(400).json({ message: "Email is already registered" });
     }
 
     // Hash the password
@@ -54,38 +56,102 @@ router.post('/signup', async (req, res) => {
     const savedStudent = await newStudent.save();
 
     // Create a JWT token for authentication
-    const token = jwt.sign({ studentId: savedStudent._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign(
+      { studentId: savedStudent._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
 
-    res.status(201).json({ message: 'Student signed up successfully', token, expiresIn: 3600, studentId: savedStudent._id});
+    res.status(201).json({
+      message: "Student signed up successfully",
+      token,
+      expiresIn: 3600,
+      studentId: savedStudent._id,
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
 // Login route for students
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
   try {
     // Check if the student exists
     const student = await Student.findOne({ email });
     if (!student) {
-      return res.status(401).json({ message: 'Invalid email or password' });
+      return res.status(401).json({ message: "Invalid email or password" });
     }
 
     // Compare passwords
     const isPasswordValid = await bcrypt.compare(password, student.password);
     if (!isPasswordValid) {
-      return res.status(401).json({ message: 'Invalid email or password' });
+      return res.status(401).json({ message: "Invalid email or password" });
     }
 
     // Generate a JWT token for authentication
-    const token = jwt.sign({ userId: student._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign({ userId: student._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    // Creating refresh token not that expiry of refresh
+    //token is greater than the access token
+
+    const refreshToken = jwt.sign(
+      {
+        userId: student._id,
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    // Assigning refresh token in http-only cookie
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      sameSite: "None",
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
 
     // Send the token in the response
-    res.json({ token, studentId: student._id, expiresIn: 3600 }); // expiresIn is in seconds (1 hour in this example)
+    res.json({ token }); // expiresIn is in seconds (1 hour in this example)
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+});
+
+router.post("/refresh", (req, res) => {
+  if (req.cookies?.jwt) {
+    // Destructuring refreshToken from cookie
+    const refreshToken = req.cookies.jwt;
+
+    // Verifying refresh token
+    jwt.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET,
+      (err, decoded) => {
+        if (err) {
+          // Wrong Refesh Token
+          return res.status(406).json({ message: "Unauthorized" });
+        } else {
+          const email = decoded.userId;
+          // Correct token we send a new access token
+          const accessToken = jwt.sign(
+            {
+              email: email,
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            {
+              expiresIn: "10m",
+            }
+          );
+          return res.json({ accessToken });
+        }
+      }
+    );
+  } else {
+    return res.status(406).json({ message: "Unauthorized" });
   }
 });
 
