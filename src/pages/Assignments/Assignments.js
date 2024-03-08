5f06296eb6f29554810f7ac767de2de4de6ff958
YukoYamano <yyamano@my.centennialcollege.ca>
2024-03-08 10:37:16 -0500

    Adding assignment is restricted for signed user only

--- src/pages/Assignments/Assignments.js
+++ src/pages/Assignments/Assignments.js
@@ -9,9 +9,11 @@ const Assignments = () => {
   const [assignments, setAssignments] = useState([]);
   const { getAuthToken } = useAuth();
   axios.defaults.headers.common["Authorization"] = `Bearer ${getAuthToken()}`;
+  const [studentName, setStudentName] = useState('');
 
   useEffect(() => {
     fetchAssignments();
+    fetchUserName();
   }, []);
 
   const fetchAssignments = async () => {
@@ -38,6 +40,22 @@ const Assignments = () => {
       console.error("Error:", error);
     }
   };
+
+  const fetchUserName = async () =>{
+    try{
+      const apiKey = process.env.REACT_APP_API_KEY;
+      const response = await axios.get(`${apiKey}/userinfo`);
+      const fetchedUserId = response.data.user.userId;
+      //console.log('fetchedUserId',fetchedUserId)
+
+      const studentResponse = await axios.get(`${apiKey}/students/${fetchedUserId}`);
+      const studentFirstName = studentResponse.data.firstName;
+      setStudentName(studentFirstName);
+      console.log('studentName',studentFirstName);
+    }catch(error){
+      console.error("Error:", error);
+    }
+  }
 
   //console.log('assignments',assignments); //null
 
@@ -74,7 +92,7 @@ const Assignments = () => {
 
   return (
     <div className="assignments-container">
-      <h2>Upcoming Assignments</h2>
+      <h2>Upcoming Assignments for {studentName}</h2>
       <Link to="/addassignment">
         <Button>Add</Button>
       </Link>
