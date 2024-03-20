663995c65633a79c28059177ea92d86af01bcfcc
Jamaal Bernabe <85321486+jnbernabe@users.noreply.github.com>
2024-03-20 13:39:06 -0400

    Refactor Assignment model schema

--- README.md
+++ README.md
@@ -1,9 +1,31 @@
 NaviGrade
 
-SRC 
+SRC
 Contains React App
 Run with npm start
 
-SERVER 
+SERVER
 contains backend for app
 run with nodemon server (requires cd in to server folder)
+
+TODO
+
+Functionality first - Issues stopping deployment -> Admin Report System - Create/ Edit User Profiles to NavBar - Student Assignment History - Dashboard Calendar - Grade Prediction Page - Ranking Assignments - Due Date Reminders - Calendar Sync Option
+
+Authentication service file
+include file with blacklist as arg -> return T or F
+update list to token to file
+
+Database Layer
+Interact with DB
+
+    methods in model to find user
+    MVC pattern
+    Search for repository
+    Restrict actions on tables
+    Repo which talks with DB to do actions
+
+Unit Test - Automated code to test functionality
+Code Quality
+
+MongoDB reminder
--- server/models/Assignment.js
+++ server/models/Assignment.js
@@ -1,15 +1,23 @@
 // models/Assignment.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const assignmentSchema = new mongoose.Schema({
-  name: {type: String, required: true},
-  dueDate: {type: Date, required: true},
-  course: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true},
-  grade:  {type: Number, default: 0, required: false },
-  weight: { type: Number, default: 1, required: false, max: 1, min: 0},
-  student: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true},
+  name: { type: String, required: true },
+  dueDate: { type: Date, required: true },
+  course: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Course",
+    required: true,
+  },
+  grade: { type: Number, default: 0, required: false, max: 100 },
+  weight: { type: Number, default: 1, required: false, max: 1, min: 0 },
+  student: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Student",
+    required: true,
+  },
 });
 
-const Assignment = mongoose.model('Assignment', assignmentSchema);
+const Assignment = mongoose.model("Assignment", assignmentSchema);
 
 module.exports = Assignment;
