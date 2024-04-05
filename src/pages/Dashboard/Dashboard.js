de29e3886b5481104ddef530ef530afee5fc88eb
YukoYamano <yyamano@my.centennialcollege.ca>
2024-04-04 21:31:50 -0400

    Email notification system and tostify system implemented

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,19 +1,17 @@
 // Dashboard.js
-
-import React from "react";
-
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Card from "react-bootstrap/Card";
+import { Card, Button } from "react-bootstrap";
 import { useAuth, AuthProvider } from "../../contexts/AuthContext";
-
 import "./Dashboard.css";
-///eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NWUxMWY2MzQyZmRjMzQzODZiNzk1MTAiLCJpYXQiOjE3MTAxMTYxOTAsImV4cCI6MTcxMDExOTc5MH0.Xhp8W-B-7VekpbVF0sFx0PL8lliy2-YyEChXGeATV9c
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import AssignmentItem from "../../components/AssignmentItem";
+
 const Dashboard = () => {
   const [assignments, setAssignments] = useState([]);
   const { getAuthToken } = useAuth();
   const { user, userDetails } = useAuth(AuthProvider);
-
   const userInfo = JSON.parse(userDetails);
   axios.defaults.headers.common["Authorization"] = `Bearer ${getAuthToken()}`;
 
@@ -25,9 +23,7 @@ const Dashboard = () => {
     const date = new Date(dateString);
     const month = date.getMonth() + 1;
     const day = date.getDate();
-    const year = date.getFullYear().toString().substr(-2); //cut first 2 digits of the year 2024->24
-    // const hours = date.getHours();
-    // const minutes = date.getMinutes().toString().padStart(2, "0"); // add leading zero if minutes < 10
+    const year = date.getFullYear().toString().substr(-2);
     return `${month}/${day}/${year} `;
   };
 
@@ -38,8 +34,6 @@ const Dashboard = () => {
         `${apiKey}/assignments/student/${userInfo.id}`
       );
       const fetchedAssignments = response.data;
-
-      // Fetch course names for each assignment
       const updatedAssignments = await Promise.all(
         fetchedAssignments
           .filter((assignment) => !assignment.completed)
@@ -51,55 +45,94 @@ const Dashboard = () => {
             return { ...assignment, course: courseName };
           })
       );
-      setAssignments(updatedAssignments);
+      const sortedAssignments = [...updatedAssignments].sort((a, b) => {
+        return new Date(a.dueDate) - new Date(b.dueDate);
+      });
+      setAssignments(sortedAssignments);
     } catch (error) {
       console.error("Error:", error);
     }
   };
 
+
+  const notifyClosestAssignment = () => {
+    if (assignments.length === 0) {
+      toast("No assignments currently.");
+      return;
+    }
+  
+    // Sort assignments by dueDate in ascending order
+    const sortedAssignments = [...assignments].sort((a, b) => {
+      return new Date(a.dueDate) - new Date(b.dueDate);
+    });
+  
+    // Get the closest assignment (first one in the sorted list)
+    const closestAssignment = sortedAssignments[0];
+  
+    const today = new Date();
+    const dueDate = new Date(closestAssignment.dueDate);
+    const diffTime = dueDate - today;
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+    // Display a toast with information about the closest assignment
+    toast(`Closest assignment: ${closestAssignment.name}  
+    ${diffDays} day(s) left`);
+  };
+
+//email notification 
+const handleSendEmail = async () => {
+  try {
+    const sortedAssignments = [...assignments].sort((a, b) => {
+      return new Date(a.dueDate) - new Date(b.dueDate);
+    });
+
+  
+    const closestAssignment = sortedAssignments[0];
+  
+    const today = new Date();
+    const dueDate = new Date(closestAssignment.dueDate);
+    const diffTime = dueDate - today;
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+    const apiKey = process.env.REACT_APP_API_KEY;
+    const response = await axios.post( `${apiKey}/email/sendEmail`,{
+    closestAssignment: closestAssignment,
+    diffDays: diffDays});
+    console.log('dueDate' ,dueDate)
+    console.log("Email sent")
+  } catch (error) {
+    console.error("Error sending email:", error);
+   
+  }
+};
+
   return (
-    // <div className="dashboard-container mx-auto">
-    //   <h2 className="display-5">{studentName}'s Dashboard </h2>
-    //   {/*<Button variant="primary">Add Assignment</Button>*/}
-    //   {assignments.length === 0 ? (
-    //     <p>No assignments currently.</p>
-    //   ) : (
-    //     <div className="assignment-list">
-    //       {assignments.map((assignment) => (
-    //         <Card key={assignment._id} className="assignment-card">
-    //           <Card.Body>
-    //             <Card.Title>{assignment.name}</Card.Title>
-    //             <Card.Text>Due Date: {formatDateToMDYY(assignment.dueDate)}</Card.Text>
-    //            {/* {studentAssignments[0]} */}
-
-    //           </Card.Body>
-    //         </Card>
-    //       ))}
-    //     </div>
-    //   )}
-    // </div>
-
     <div className="dashboard-container mx-auto">
       <h3 className="display-5"> Dashboard for {userInfo.firstName} </h3>
 
+      <Button variant="warning" onClick={notifyClosestAssignment}>
+        What should I do!?
+      </Button>
+      <ToastContainer />
+      <button onClick={handleSendEmail}>Send Email</button>
+      <h4>Assignment(s)</h4>
       {assignments.length === 0 ? (
         <p>No assignments currently.</p>
       ) : (
         <div className="assignment-list">
-          <p>Assignments</p>
-          <ul>
-            {assignments.map((assignment) => (
-              <li key={assignment._id}>
-                <div>
-                  <h4>{assignment.name}</h4>
-                  <h4>
-                    Grade: {assignment.grade !== 0 ? assignment.grade : "TBD"}
-                  </h4>
-                  <p>Due Date: {formatDateToMDYY(assignment.dueDate)}</p>
-                </div>
-              </li>
-            ))}
-          </ul>
+          {assignments.map((assignment) => (
+            <Card
+              key={assignment._id}
+              className="assignment-card"
+              style={{ flex: "0 0 calc(33% - 1em)", margin: "0.5em" }}
+              bsPrefix
+            >
+              <AssignmentItem
+                assignment={assignment}
+                studentId={userInfo.id}
+              />
+            </Card>
+          ))}
         </div>
       )}
     </div>
